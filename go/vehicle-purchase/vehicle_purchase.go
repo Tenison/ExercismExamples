dcb1ccaa1b2d60f1cd62980bab8ef76d4fdbd60e
Tenison <tenisonoseiowusu@gmail.com>
2022-04-12 16:50:15 +0000

    needslicense and choosevehicle functions implemented

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -2,12 +2,20 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	if option1 > option2 {
+		return option1
+	}
+
+	return option2
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
