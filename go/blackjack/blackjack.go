478804e2dc7f8bd2912ecd4b31021e44fde8b892
Tenison <tenisonoseiowusu@gmail.com>
2022-04-22 09:21:35 +0000

    go blackjack execise completed

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -27,8 +27,41 @@ func ParseCard(card string) int {
 	return 0
 }
 
-// FirstTurn returns the decision for the first turn, given two cards of the
-// player and one card of the dealer.
+func IsBlackjack(card1, card2 string) bool {
+	return ParseCard(card1)+ParseCard(card2) == 21
+}
+
+// LargeHand implements the decision tree for hand scores larger than 20 points.
+func LargeHand(isBlackjack bool, dealerScore int) string {
+	if !isBlackjack {
+		return "P"
+	}
+	if dealerScore < 10 {
+		return "W"
+	}
+	return "S"
+}
+
+// SmallHand implements the decision tree for hand scores with less than 21 points.
+func SmallHand(handScore, dealerScore int) string {
+	if handScore >= 17 {
+		return "S"
+	} else if handScore <= 11 {
+		return "H"
+	} else if handScore >= 12 && handScore <= 16 && dealerScore >= 7 {
+		return "H"
+	}
+	return "S"
+}
+
+// FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
+// This function is already implemented and does not need to be edited. It pulls the other functions together in a
+// complete decision tree for the first turn.
 func FirstTurn(card1, card2, dealerCard string) string {
-	return "not written"
+	handScore := ParseCard(card1) + ParseCard(card2)
+	dealerScore := ParseCard(dealerCard)
+	if 20 < handScore {
+		return LargeHand(IsBlackjack(card1, card2), dealerScore)
+	}
+	return SmallHand(handScore, dealerScore)
 }
